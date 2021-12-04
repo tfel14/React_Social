924c0a0d455fee55b6e6a259190009b4bdb425cd
nathan-ashman <natetheman19@gmail.com>
2021-12-04 06:40:07 -0500

    linked user posts to database

--- social-app/src/components/feed/Feed.jsx
+++ social-app/src/components/feed/Feed.jsx
@@ -2,21 +2,37 @@ import Share from "../share/Share";
 import Post from "../post/Post"
 import "./feed.css";
 import {Posts} from "../../dummydata";
-// import React, {useState} from 'react';
-function Feed() {
-    let DBPosts = fetch('http://localhost:9999/api/post').then((res)=>res.json());
-    console.log(JSON.parse(JSON.stringify(DBPosts)));
-    // let [postList, postHandler] = useState(null);
-    // postHandler(list);
-        return (
-        <div className="feed">
-            <div className="feedWrapper">
-                <Share/>
-                {Posts.map((p)=> (
+import React, {Component, useRef} from 'react';
+class Feed extends Component {
+    constructor(){
+        super();
+        this.state = {posts: [], mappedPosts: []}
+    }
+    async componentDidMount() {
+        let DBPosts = await fetch('http://localhost:9999/api/post').then((res)=>res.json());
+        this.setState({ data: DBPosts });
+        let postList = this.state.data;
+        if(postList != undefined){
+            this.setState({mappedPosts: postList.map((p)=> (
                 <Post key={p.id} post={p} />
-                ))} 
+            ))})
+        }
+    }
+    
+    render(){
+        let mappedPostList = this.state.mappedPosts;
+            return (
+            <div className="feed">
+                <div className="feedWrapper">
+                    <Share/>
+                    {/* {Posts.map((p)=> (
+                    <Post key={p.id} post={p} />
+                    ))} */}
+                    {mappedPostList}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    
 }
 export default Feed;
--- social-app/src/components/post/Post.jsx
+++ social-app/src/components/post/Post.jsx
@@ -1,44 +1,61 @@
 import "./post.css";
 import { MoreVert } from "@material-ui/icons";
 import { Users } from "../../dummydata";
-import { useState } from "react"
+import React, {Component, useRef} from 'react';
 
-export default function Post({post}) {
-    const [like,setLike] = useState(post.like)
-    const [isLiked,setIsLiked] = useState(false)
 
-    const likeHandler = ()=>{
-        setLike(isLiked ? like-1 : like+1)
-        setIsLiked(!isLiked)
+class Post extends Component {
+    constructor(){
+        super();
+        this.state = {like: 0, isLiked: false, users: []}
     }
-    return (
-        <div className="post" >
-          <div className="postWrapper">
-              <div className="postTop">
-                  <div className="postTopLeft">
-                      <img className="postProfileImg" src={Users.filter((u) => u.id === post?.userId)[0].profilePicture} alt=""/>
-                      <span className="postUsername">{Users.filter((u) => u.id === post.userId)[0].username } </span>
-                      <span className="postDate">{post.date}</span>
+    async componentDidMount(){
+        let users = await fetch('http://localhost:9999/api/post').then((res)=>res.json());
+        this.setState({users});
+    }
+    render(){
+        let post = this.props.post;
+        // const [like,setLike] = useState(post.like)
+        // const [isLiked,setIsLiked] = useState(false)
+        console.log(post);
+        const likeHandler = ()=>{
+            this.setState({
+                like: this.state.isLiked ? this.like-1 : this.like + 1
+            })
+            this.setState({isLiked: false})
+        }
+        let userList = this.state.users;
+        return (
+            <div className="post" >
+              <div className="postWrapper">
+                  <div className="postTop">
+                      <div className="postTopLeft">
+                          <img className="postProfileImg" src={userList.filter((u) => u._id === post?.userid).pfp} alt=""/>
+                          <span className="postUsername">{Users.filter((u) => u._id === post?.userid).username } </span>
+                          <span className="postDate">{post.date}</span>
+                      </div>
+                      <div className="postTopRight">
+                        <MoreVert />
+                      </div>
                   </div>
-                  <div className="postTopRight">
-                    <MoreVert />
+                  <div className="postCenter">
+                      <span className="postText">{post.desc}</span>
+                      <img className="postImg" src={post.photo} alt=""/>
+                  </div>
+                  <div className="postBottom">
+                      <div className="postBottomLeft">
+                          <img className="likeIcon" src="/assets/1.png" onClick={likeHandler} alt=""/>
+                          <img className="likeIcon" src="/assets/2.png" onClick={likeHandler} alt=""/>
+                          <span className="postLikeCounter">{this.state.like} people like it</span>
+                      </div>
+                      <div className="postBottomRight">
+                          <span className="postCommentText">{post.comment} comments</span>
+                      </div>
                   </div>
               </div>
-              <div className="postCenter">
-                  <span className="postText">{post.desc}</span>
-                  <img className="postImg" src={post.photo} alt=""/>
-              </div>
-              <div className="postBottom">
-                  <div className="postBottomLeft">
-                      <img className="likeIcon" src="/assets/1.png" onClick={likeHandler} alt=""/>
-                      <img className="likeIcon" src="/assets/2.png" onClick={likeHandler} alt=""/>
-                      <span className="postLikeCounter">{like} people like it</span>
-                  </div>
-                  <div className="postBottomRight">
-                      <span className="postCommentText">{post.comment} comments</span>
-                  </div>
-              </div>
-          </div>
-        </div>
-    )
+            </div>
+        )
+    }
+    
 }
+export default Post;
